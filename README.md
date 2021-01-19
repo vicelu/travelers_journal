61d662d7b6e2b9fe44101259afaabe69b6d39667
Vicko Lukšić <vickoluksic@atron.de>
2021-01-20 00:47:04 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# travelers_journal
+# Travelers journal
+
+## Fancy interactive log of visited places
+
+### DeckGL on top of Mapbox as frontend, with Node.JS backend server pulling data from PostGIS spatial database
---
+++ app.ts
@@ -0,0 +1,11 @@
+// import mapboxgl from 'mapbox-gl';
+ 
+// mapboxgl.accessToken = '<your access token here>';
+// const map = new mapboxgl.Map({
+// container: 'map',
+// style: 'mapbox://styles/mapbox/streets-v11',
+// center: [-74.5, 40],
+// zoom: 4
+// });
+
+console.log('script was loaded');
